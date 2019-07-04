f043bc8c232d8afad6b1c9969500f5a61d28cbc6
Billy Vlachos <williamcaminiti@live.com>
2019-07-04 15:41:21 +0200

    Added required fields upon create

--- src/schema/schema.js
+++ src/schema/schema.js
@@ -13,7 +13,7 @@ const Author = require('./../models/author');
 // Define object types
 const {
     GraphQLObjectType, GraphQLString, GraphQLSchema,
-    GraphQLID, GraphQLInt, GraphQLList
+    GraphQLID, GraphQLInt, GraphQLList, GraphQLNonNull
 } = graphql;
 
 // Dummy Data
@@ -165,8 +165,8 @@ const Mutation = new GraphQLObjectType({
         addAuthor: {
             type: AuthorType,
             args: {
-                name: { type: GraphQLString },
-                age: { type: GraphQLInt }
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                age: { type: new GraphQLNonNull(GraphQLInt) }
             },
             resolve(parent, args) {
                 // Create author locally
@@ -183,9 +183,9 @@ const Mutation = new GraphQLObjectType({
         addBook: {
             type: BookType,
             args: {
-                name: { type: GraphQLString },
-                genre: { type: GraphQLString },
-                authorId: { type: GraphQLID },
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                genre: { type: new GraphQLNonNull(GraphQLString) },
+                authorId: { type: new GraphQLNonNull(GraphQLID) },
             },
             resolve(parent, args) {
                 return Book.create({
