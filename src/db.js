d604f8087da5d607f22d044d4324bc186a9645d5
Billy Vlachos <williamcaminiti@live.com>
2019-07-05 14:23:34 +0200

    Update start script to run as dev

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 
 // Define database url and initialize sequelize
 const databaseUrl = process.env.DATABASE_URL | 'postgres://postgres:secret@localhost:5432/postgres';
-const sequelize = new Sequelize(databaseUrl);
+const sequelize = new Sequelize(databaseUrl, { dialect: 'postgres' });
 
 // Configure Sequelize without sync
 sequelize
