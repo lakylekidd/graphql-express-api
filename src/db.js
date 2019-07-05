e3343a7d00c08bea416563f170b80a0a5100e6c1
Billy Vlachos <williamcaminiti@live.com>
2019-07-05 14:12:20 +0200

    Repushing

--- src/db.js
+++ src/db.js
@@ -5,7 +5,7 @@ const Sequelize = require('sequelize');
 const databaseUrl = process.env.DATABASE_URL | 'postgres://postgres:secret@localhost:5432/postgres';
 const sequelize = new Sequelize(databaseUrl);
 
-// Configure Sequelize
+// Configure Sequelize without sync
 sequelize
     .sync()
     .then(() => console.log('Database schema updated'))
